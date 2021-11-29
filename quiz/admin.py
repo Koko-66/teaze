dfca6fb5d94c19105b4017c850f4b963ce813024
Koko-66 <k.cisek@gmail.com>
2021-11-29 22:27:39 +0000

    Revert "Last commit"
    This reverts commit 1bb3d75f6103c3213fdff2842169ab1922c10bde.

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Category, Question
-from django_summernote.admin import SummernoteModelAdmin
 
-
-admin.site.register(Category)
-
-@admin.register(Question)
-class QuestionAdmin(SummernoteModelAdmin):
-
-    summernote_fields = ('body')
+# Register your models here.
