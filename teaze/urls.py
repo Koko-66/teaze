65c11e45a6d6688b166ab77ddef80aa06b9049e5
Koko-66 <k.cisek@gmail.com>
2021-12-15 09:14:09 +0000

    Add allauth and links to signup, login and logout.

--- teaze/urls.py
+++ teaze/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls'), name='quiz_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
