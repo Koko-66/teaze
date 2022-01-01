9e2e091f040cf174ce25098413ccfa164292ea45
Koko-66 <k.cisek@gmail.com>
2022-01-01 22:10:16 +0000

    Change index.html to dashboard view and create manage quizzes view and template.
    Addfunction to toggle quiz status.

--- quiz/urls.py
+++ quiz/urls.py
@@ -8,15 +8,19 @@ from quiz.views import (
     QuizDetailsView,
     EditQuizView,
     DeleteQuizView,
+    toggle_status,
+    welcome_page_view,
 )
 
 app_name = 'quiz'
 urlpatterns = [
-    path('', QuizListView.as_view(), name='home'),
+    path('', welcome_page_view, name='home'),
+    path('quizzes/manage_quizzes', QuizListView.as_view(), name='manage_quizzes'),
     # path('add_quiz/', AddQuizView.as_view(), name='add_quiz'),
     path('add_quiz/', add_quiz_view, name='add_quiz'),
-    path('<slug:slug>/details', QuizDetailsView.as_view(), name='quiz_details'),
+    path('<slug:slug>/details/', QuizDetailsView.as_view(), name='quiz_details'),
     path('<slug:slug>/', TakeQuizView.as_view(), name='take_quiz'),
     path('<slug:slug>/edit_quiz/', EditQuizView.as_view(), name='edit_quiz'),
     path('<slug:slug>/delete_quiz/', DeleteQuizView.as_view(), name='delete_quiz'),
+    path('<slug:slug>/toggle/', toggle_status, name='toggle'),
     ]
--- quiz/views.py
+++ quiz/views.py
@@ -9,6 +9,8 @@ from django.views.generic import (
 )
 from .models import Quiz
 from .forms import NewQuizForm
+from categories.models import Category
+from questions.models import Question
 from results.models import Assessment
 
 
@@ -46,7 +48,7 @@ class EditQuizView(UpdateView):
     queryset = Quiz.objects.all()
 
     def get_object(self):
-        slug = self.kwargs.get("slug")
+        slug = self.kwargs.get('slug')
         return get_object_or_404(Quiz, slug=slug)
 
     def form_valid(self, form):
@@ -59,7 +61,7 @@ class DeleteQuizView(DeleteView):
     template = 'quiz/delete_quiz.html'
 
     def get_object(self):
-        slug = self.kwargs.get("slug")
+        slug = self.kwargs.get('slug')
         return get_object_or_404(Quiz, slug=slug)
 
     def get_success_url(self):
@@ -69,7 +71,7 @@ class DeleteQuizView(DeleteView):
 class QuizListView(generic.ListView):
     model = Quiz
     # queryset = Quiz.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'quiz/manage_quizzes.html'
 
     def get(self, request):
         if request.user.groups.filter(name='Admin').exists():
@@ -98,7 +100,7 @@ class QuizDetailsView(DetailView):
     template = 'quiz/quiz_detail.html'
 
     def get_object(self):
-        slug = self.kwargs.get("slug")
+        slug = self.kwargs.get('slug')
         return get_object_or_404(Quiz, slug=slug)
 
 
@@ -115,8 +117,59 @@ class TakeQuizView(View):
             questions.append({question: options})
 
         context = {
-            "questions": questions,
-            "quiz": quiz,
+            'questions': questions,
+            'quiz': quiz,
         }
 
-        return render(request, "quiz/take_quiz.html", context)
+        return render(request, 'quiz/take_quiz.html', context)
+
+
+def toggle_status(request, slug):
+    quiz = get_object_or_404(Quiz, slug=slug)
+    if quiz.status != 0:
+        quiz.status = 0
+    else:
+        quiz.status = 1
+    quiz.save()
+    return redirect('../details')
+
+
+def welcome_page_view(request):
+    quiz_draft_count = Quiz.objects.filter(status=0).count()
+    quiz_published_count = Quiz.objects.filter(status=1).count()
+    question_draft_count = Question.objects.filter(status=0).count()
+    question_published_count = Question.objects.filter(status=1).count()
+    categories_count = Category.objects.all().count()
+
+    if request.user.groups.filter(name='Admin').exists():
+        quiz_list = Quiz.objects.order_by('-created_on')
+    else:
+        quiz_list = Quiz.objects.filter(status=1).order_by('-created_on')
+
+    if request.user.is_authenticated:
+        assessments = Assessment.objects.filter(user=request.user)
+        completed_quizzes = []
+        for assessment in assessments:
+            completed_quizzes.append(assessment.quiz)
+
+        # return render(request, self.template_name, {
+        #     'completed_quizzes': completed_quizzes,
+        #     'assessments': assessments,
+        #     'quiz_list': quiz_list,
+        #     }
+        # )
+    else:
+        return redirect('account_login')
+
+    context = {
+        'quiz_draft_count': quiz_draft_count,
+        'quiz_published_count': quiz_published_count,
+        'question_draft_count': question_draft_count,
+        'question_published_count': question_published_count,
+        'categories_count': categories_count,
+
+        'completed_quizzes': completed_quizzes,
+        'assessments': assessments,
+        'quiz_list': quiz_list,
+    }
+    return render(request, 'index.html', context)
