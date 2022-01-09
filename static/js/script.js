11e9387b2fa11b732b2d9b337c15f319f5a2552a
Koko-66 <k.cisek@gmail.com>
2022-01-09 13:34:06 +0000

    Add script to handle category adding and deletion in modals via django-bootstrap-modal-forms.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
-$( document ).ready(function() {
-    // $('#myModal').modal(options)
-});
-console.log('Hello world')
+// console.log('Hello world')
 
+$( document ).ready(function () {
+    // Delete category buttons - formURL is retrieved from the data of the element
+    $(".delete-category").each(function () {
+        $(this).modalForm({formURL: $(this).data("form-url"), isDeleteForm: true});
+    });
+
+    // Add category button opens form in modal with id="add-category-modal"
+    $("#add-category").modalForm({
+        formURL: "/categories/add_category/",
+        modalID: "#add-category-modal"
+    });
+});
