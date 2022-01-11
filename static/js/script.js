bc81727a17ad67466f0b7276f25999071dc0fa65
Koko-66 <k.cisek@gmail.com>
2022-01-11 19:14:55 +0000

    Add functions for managing questions and quizzes.

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,47 @@
 // console.log('Hello world')
 
 $( document ).ready(function () {
+    // QUIZZES - MODALS //
+    // Add quiz link opens form in modal with id="add-question-modal"
+    $("#add-quiz").modalForm({
+        formURL: "/quiz/add_quiz/",
+        modalID: "#add-quiz-modal"
+    });
+
+    // Delete quiz button - formURL is retrieved from the data of the element
+    $(".delete-quiz").each(function () {
+        $(this).modalForm({formURL: $(this).data("form-url"), isDeleteForm: true});
+    });
+
+    // Edit quiz button - formURL is retrieved from the data of the element
+    $(".edit-quiz").each(function () {
+        $(this).modalForm({formURL: $(this).data("form-url")});
+    });
+
+    // QUESTIONS - MODALS //
+    // Add question link opens form in modal with id="add-question-modal"
+    $("#add-question").modalForm({
+        formURL: "/questions/add_question/",
+        modalID: "#add-question-modal"
+    });
+
+    // Delete question button - formURL is retrieved from the data of the element
+    $(".delete-question").each(function () {
+        $(this).modalForm({formURL: $(this).data("form-url"), isDeleteForm: true});
+    });
+
+    // Edit question button - formURL is retrieved from the data of the element
+    $(".edit-question").each(function () {
+        $(this).modalForm({formURL: $(this).data("form-url")});
+    });
+
+    // CATEGORIES - MODALS //
+    // Add category link opens form in modal with id="add-category-modal"
+    $("#add-category").modalForm({
+        formURL: "/categories/add_category/",
+        modalID: "#add-category-modal"
+    });
+
     // Delete category buttons - formURL is retrieved from the data of the element
     $(".delete-category").each(function () {
         $(this).modalForm({formURL: $(this).data("form-url"), isDeleteForm: true});
@@ -11,9 +52,4 @@ $( document ).ready(function () {
         $(this).modalForm({formURL: $(this).data("form-url")});
     });
 
-    // Add category button opens form in modal with id="add-category-modal"
-    $("#add-category").modalForm({
-        formURL: "/categories/add_category/",
-        modalID: "#add-category-modal"
-    });
 });
