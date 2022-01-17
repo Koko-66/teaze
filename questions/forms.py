725dd7629e562c0f14b4436849fe8dca60f5f69c
Koko-66 <k.cisek@gmail.com>
2022-01-17 22:05:01 +0000

    Fix categories not saving when adding/editing question.
    Add save_m2m() to the form savemethod.
    
    Add save method to AddOption form to logged in user as author.

--- questions/forms.py
+++ questions/forms.py
@@ -1,10 +1,13 @@
 """Form to create questions and answers"""
 from django import forms
+from django.forms import widgets
 from django.contrib.auth.models import User
+
 from bootstrap_modal_forms.mixins import (
     PopRequestMixin,
     CreateUpdateAjaxMixin
 )
+from categories.models import Category
 from .models import Question, Option
 
 
@@ -28,9 +31,6 @@ class NewQuestionForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
     
     class Meta:
         model = Question
-        help_texts = {
-            'category': 'Hold Shift to select more than one category.',
-        }
         fields = (
             'body',
             'quiz',
@@ -38,6 +38,33 @@ class NewQuestionForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
             'status',
             'feedback',
             'category',
+        )
+    
+    def save(self, commit=False):
+        """
+        Overwrites the default save method to add current user as author and
+        prevents Ajax error: 'NoneType' object has no attribute 'META'
+        """
+
+        # code from https://stackoverflow.com/questions/54048741/nonetype-object-has-no-attribute-is-ajax
+        if not self.request.is_ajax():
+            instance = super(CreateUpdateAjaxMixin, self).save(commit=commit)
+            instance.author = User.objects.get(pk=self.request.user.pk)
+            instance.save()
+            self.save_m2m()
+        else:
+            instance = super(CreateUpdateAjaxMixin, self).save(commit=False)
+        return instance
+
+
+class NewOptionForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
+
+    class Meta:
+        model = Option
+        fields = (
+            'option',
+            'is_correct',
+            'position',
         )
 
     def save(self, commit=False):
@@ -52,16 +79,3 @@ class NewQuestionForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
         else:
             instance = super(CreateUpdateAjaxMixin, self).save(commit=False)
         return instance
-
-    
-    
-
-class NewOptionForm(forms.ModelForm):
-
-    class Meta:
-        model = Option
-        fields = (
-            'option',
-            'is_correct',
-            'position',
-        )
