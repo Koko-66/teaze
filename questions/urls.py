05b533eeb99e70e842154eb7a6c02ee150ebfbd9
Koko-66 <k.cisek@gmail.com>
2022-01-20 08:53:49 +0000

    Uncomment paths for add question and option function-based views; reorder.

--- questions/urls.py
+++ questions/urls.py
@@ -3,30 +3,28 @@ from django.urls import path
 from .views import (
     AddQuestionView,
     add_question_view,
-    # AddOptionView,
+    AddOptionView,
     add_option_view,
     QuestionDetailsView,
     EditQuestionView,
     DeleteQuestionView,
-    # question_delete_view,
     QuestionListView,
-    toggle_status,
+    toggle_question_status,
 )
 
 app_name = 'questions'
 
 urlpatterns = [
+    # urls for questions from question management views
     path('questions/', QuestionListView.as_view(), name='manage_questions'),
+    path('questions/<int:pk>/', QuestionDetailsView.as_view(), name='question_details'),
     path('questions/add_question/', AddQuestionView.as_view(), name='add_question'),
-    path('<slug:slug>/add_question/', add_question_view, name='add_question_to_quiz'),
-    path('<slug:slug>/<int:question_id>/add_option/', add_option_view, name='add_option'),
-    path('questions/<int:question_id>/', QuestionDetailsView.as_view(), name='question_details'),
-    path('questions/<int:question_id>/edit_question/', EditQuestionView.as_view(), name='edit_question'),
-    path('questions/<int:question_id>/', QuestionDetailsView.as_view(), name='question_details'),
+    path('questions/edit_question/<int:pk>/', EditQuestionView.as_view(), name='edit_question'),
     path('questions/delete_question/<int:pk>/', DeleteQuestionView.as_view(), name='delete_question'),
-    path('questions/toggle_question/<int:pk>/', toggle_status, name='toggle_question'),
-
-    # path('questions/<int:question_id>/delete_question/', DeleteQuestionView.as_view(), name='delete_question'),
-    
-
+    path('questions/toggle/', toggle_question_status, name='toggle_question'),
+    # urls for questions from question management views
+    path('<slug:slug>/add_question/', add_question_view, name='add_question_in_quiz'),
+    # urls for options
+    path('<slug:slug>/<int:question_id>/add_option/', add_option_view, name='add_option_in_quiz'),
+    path('questions/<int:pk>/add_option', AddOptionView, name='add_option'),
 ]
