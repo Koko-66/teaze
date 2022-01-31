ebea495acc12ae601645098e3686d1026d476173
Koko-66 <k.cisek@gmail.com>
2022-01-31 20:22:07 +0000

    Fix line lengths.

--- categories/urls.py
+++ categories/urls.py
@@ -9,8 +9,12 @@ from categories.views import (
 
 app_name = 'categories'
 urlpatterns = [
-    path('categories/', CategoriesListView.as_view(), name='manage_categories'),
-    path('categories/add_category/', AddCategoryView.as_view(), name='add_category'),
-    path('categories/edit_category/<int:pk>/', EditCategoryView.as_view(), name='edit_category'),
-    path('categories/delete_category/<int:id>/', DeleteCategoryView.as_view(), name='delete_category'),
+    path('categories/', CategoriesListView.as_view(),
+         name='manage_categories'),
+    path('categories/add_new_category/', AddCategoryView.as_view(),
+         name='add_category'),
+    path('categories/edit_category/<int:pk>/', EditCategoryView.as_view(),
+         name='edit_category'),
+    path('categories/delete_category/<int:id>/', DeleteCategoryView.as_view(),
+         name='delete_category'),
     ]
