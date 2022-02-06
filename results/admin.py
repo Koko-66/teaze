06c008ee1397c34924c00e26605a34daa5e1689c
Koko-66 <k.cisek@gmail.com>
2022-02-06 10:51:17 +0000

    Update display in django admin to include date and show answers inline.

--- results/admin.py
+++ results/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from results.models import Assessment, Answer
 
-admin.site.register(Assessment)
-admin.site.register(Answer)
+
+class AnswerInline(admin.TabularInline):
+    """Show answers inline with Assessment in django admin."""
+    model = Answer
+    # prepopulated_fields = {"answer": ("answer",)}
+    list_filter = ('created_on', 'category')
+    search_fields = ('category')
+
+
+class AssessmentAdmin(admin.ModelAdmin):
+    """Pull answers for the assessment inline in django admin."""
+    inlines = [AnswerInline]
+    list_display = (str, 'created_on')
+
+
+admin.site.register(Assessment, AssessmentAdmin)
+
