52d5a7dabfccbe0a6f0489bd6c099d443ba5e70a
Koko-66 <k.cisek@gmail.com>
2022-02-21 00:36:01 +0000

    Add toggle_status method to model.

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,5 +1,9 @@
 """Forms for quiz app"""
 from django import forms
+from bootstrap_modal_forms.mixins import (
+    PopRequestMixin,
+    CreateUpdateAjaxMixin
+)
 from questions.models import Question
 from .models import Quiz
 
@@ -37,4 +41,16 @@ class AddQuestionToQuizForm(forms.ModelForm):
         fields = (
             'body',
             'feedback',
+        )
+
+class ToggleQuizStatusForm(PopRequestMixin, CreateUpdateAjaxMixin, forms.ModelForm):
+    """Form to create new quiz."""
+
+    STATUS = ((0, 'Draft'), (1, 'Published'))
+
+    class Meta:
+        """Meta class specifying fields."""
+        model = Quiz
+        fields = (
+            'status',
         )
